0313df14b04fda0555915832bd9f4f6542db1cd9
ShinMini <77220824+ShinMini@users.noreply.github.com>
2023-03-15 15:16:15 +0900

    fix: overlaped header
    line11: remove above one

--- src/pages/MapDetailPage.tsx
+++ src/pages/MapDetailPage.tsx
@@ -1,20 +1,27 @@
 import {IonPage, IonHeader, IonToolbar, IonTitle, IonContent} from '@ionic/react'
 import {RouteComponentProps} from 'react-router'
-
+import {IonIcon} from '@ionic/react'
+import {chevronBackOutline, chevronForwardOutline} from 'ionicons/icons'
 interface MapDetailPageProps
 	extends RouteComponentProps<{
 		id: string
 	}> {}
 
 const MapDetailPage: React.FC<MapDetailPageProps> = ({match}) => {
+	console.log(match)
 	return (
 		<IonPage>
 			<IonHeader>
 				<IonToolbar>
-					<IonTitle>Map Detail</IonTitle>
+					<IonTitle>Direction {match.params.id}</IonTitle>
 				</IonToolbar>
 			</IonHeader>
-			<IonContent>Direction {match.params.id}</IonContent>
+
+			<IonContent>
+				<IonIcon icon={chevronBackOutline} color='white' />
+				Direction {match.params.id}
+				<IonIcon icon={chevronForwardOutline} color='white' />
+			</IonContent>
 		</IonPage>
 	)
 }
