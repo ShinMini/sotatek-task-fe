28f99e7d88e9028609b93c8f9dc379704c236002
ShinMini <77220824+ShinMini@users.noreply.github.com>
2023-03-16 11:35:05 +0900

    feat: create feature about move around in map
    1. feat: arrow button
    2. feat: create store state for manage img data made json format
    3. feat: control element class for animation

--- src/pages/MapDetailPage.tsx
+++ src/pages/MapDetailPage.tsx
@@ -4,11 +4,7 @@ import {
 	IonToolbar,
 	IonTitle,
 	IonContent,
-	IonButton,
 	IonImg,
-	IonItem,
-	IonLabel,
-	IonList,
 	IonGrid,
 	IonCol,
 	IonRow
@@ -18,14 +14,70 @@ import {IonIcon} from '@ionic/react'
 import {chevronBackOutline, chevronForwardOutline, homeOutline} from 'ionicons/icons'
 
 import img1 from '../assets/img/1.jpg'
+import img2 from '../assets/img/2.jpg'
+import img3 from '../assets/img/3.jpg'
+import img4 from '../assets/img/4.jpg'
+import img5 from '../assets/img/5.jpg'
+import img6 from '../assets/img/6.jpg'
+import img7 from '../assets/img/7.jpg'
+import img8 from '../assets/img/8.jpg'
+import img9 from '../assets/img/9.jpg'
+import img10 from '../assets/img/10.jpg'
+import houseImg from '../assets/img/house.jpg'
+
+import {useEffect, useRef, useState} from 'react'
+import imgDataSet from '../assets/data/imgData.json'
 
 interface MapDetailPageProps
 	extends RouteComponentProps<{
 		id: string
 	}> {}
 
+const imgSet = {
+	img1,
+	img2,
+	img3,
+	img4,
+	img5,
+	img6,
+	img7,
+	img8,
+	img9,
+	img10,
+	house: houseImg
+}
 const MapDetailPage: React.FC<MapDetailPageProps> = ({match, history}) => {
-	console.log(match)
+	const [imgSrc, setImgSrc] = useState(imgSet.img1)
+	const [imgPosition, setImgPosition] = useState(1)
+	const [prevImgPosition, setPrevImgPosition] = useState(imgPosition)
+	const imgRef = useRef<HTMLIonImgElement>(null)
+
+	useEffect(() => {
+		const currentImgData = imgDataSet.scenes[Number(match.params.id) - 1]
+
+		const imgUrl = currentImgData.background_url as keyof typeof imgSet
+		const imgHitzones = currentImgData.hitzones
+
+		console.log(imgHitzones)
+
+		setImgSrc(imgSet[imgUrl])
+	}, [match.params.id])
+
+	useEffect(() => {
+		if (imgPosition === 0) {
+			imgRef.current!.className = 'img img-MiddleToLeft'
+			imgRef.current!.style.objectPosition = '0% 50%'
+		} else if (imgPosition === 1) {
+			if (prevImgPosition === 0) imgRef.current!.className = 'img img-LeftToMiddle'
+			else if (prevImgPosition === 2) imgRef.current!.className = 'img img-RightToMiddle'
+			imgRef.current!.style.objectPosition = '50% 50%'
+		} else if (imgPosition === 2) {
+			imgRef.current!.className = 'img img-MiddleToRight'
+			imgRef.current!.style.objectPosition = '100% 50%'
+		}
+		setPrevImgPosition(imgPosition)
+	}, [imgPosition, prevImgPosition])
+
 	return (
 		<IonPage>
 			<IonHeader>
@@ -49,9 +101,21 @@ const MapDetailPage: React.FC<MapDetailPageProps> = ({match, history}) => {
 			</IonHeader>
 
 			<IonContent>
-				<IonIcon icon={chevronBackOutline} className='absolute left-arrow' />
-				<IonIcon icon={chevronForwardOutline} className='absolute right-arrow' />
-				<IonImg className='img' src={img1} />
+				{imgPosition !== 0 && (
+					<IonIcon
+						icon={chevronBackOutline}
+						className='absolute left-arrow'
+						onClick={() => setImgPosition(prev => (prev -= 1))}
+					/>
+				)}
+				{imgPosition !== 2 && (
+					<IonIcon
+						icon={chevronForwardOutline}
+						className='absolute right-arrow'
+						onClick={() => setImgPosition(prev => (prev += 1))}
+					/>
+				)}
+				<IonImg ref={imgRef} className='img' src={imgSrc} />
 			</IonContent>
 		</IonPage>
 	)
